09d34e09db752a9ec7f0ee831280e019ed0cc239
Benny Zhu <developer.benny.zhu@proton.me>
2026-09-24 20:22:15 -0400

    feature: schedule robot centric command when any joystick is moved

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -2,10 +2,22 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.Robot;
 
 @TeleOp(name = "Base TeleOp")
 public class BaseTeleOp extends CommandOpMode {
+
+    private FollowerSubsystem followerSubsystem;
     @Override
     public void initialize() {
+
+    }
+    public void scheduleRobotCentricIfAnyJoystickIsMoved() {
+        double deadZone = 0.05;
+        boolean moved = Math.abs(gamepad1.left_stick_x) > deadZone || Math.abs(gamepad1.left_stick_y) > deadZone || Math.abs(gamepad1.right_stick_x) > deadZone || Math.abs(gamepad1.right_stick_y) > deadZone;
+        if (moved) {
+            RobotCentricCommand robotCentricCommand = new RobotCentricCommand(followerSubsystem);
+            robotCentricCommand.schedule(true);
+        }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoldPoseCommand.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.seattlesolvers.solverslib.pedroCommand.HoldPointCommand;
+
+public class HoldPoseCommand extends HoldPointCommand {
+    public HoldPoseCommand(FollowerSubsystem followerSubsystem) {
+
+    }
+}
