47cccd452f14e582e6216463e31b3a7ccfffdce6
Benny Zhu <bennyzhu22009@gmail.com>
2026-09-24 20:53:07 -0400

    feature: go to pose when press A

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -1,16 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.math.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.Robot;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
 @TeleOp(name = "Base TeleOp")
 public class BaseTeleOp extends CommandOpMode {
-
     private FollowerSubsystem followerSubsystem;
+    GamepadEx gamepad = new GamepadEx(gamepad1);
     @Override
     public void initialize() {
-
+        followerSubsystem = new FollowerSubsystem();
+        gamepad.getGamepadButton(GamepadKeys.Button.A).whenPressed(new GoToPoseCommand(followerSubsystem, new Pose(50, 50)));
+    }
+    @Override
+    public void run() {
+        scheduleRobotCentricIfAnyJoystickIsMoved();
     }
     public void scheduleRobotCentricIfAnyJoystickIsMoved() {
         double deadZone = 0.05;
