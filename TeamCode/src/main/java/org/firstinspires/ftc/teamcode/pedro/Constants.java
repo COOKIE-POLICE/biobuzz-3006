e57bd870b2274b42b4401c1267e3f48893e5d2eb
Benny Zhu <bennyzhu22009@gmail.com>
2026-09-24 21:44:13 -0400

    feature: set up pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,36 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.algorithm.Foresight;
+import com.pedropathing.algorithm.ForesightConfig;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.pedropathing.revhub.localizers.PinpointConfig;
+import com.pedropathing.revhub.localizers.PinpointLocalizer;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
-        return null;
+        return new Follower(
+                new PinpointLocalizer(h, pinpointConfig),
+                new Mecanum(h, mecanumConfig),
+                new Foresight(foresightConfig)
+        );
     }
+    public static MecanumConfig mecanumConfig = new MecanumConfig(
+            c -> {
+                c.frontLeftName.set("fl");
+                c.backLeftName.set("bl");
+                c.frontRightName.set("fr");
+                c.backRightName.set("br");
+                c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+                c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+            }
+    );
+    public static PinpointConfig pinpointConfig = new PinpointConfig(c -> {
+    });
+    public static ForesightConfig foresightConfig = new ForesightConfig(c -> {});
 }
